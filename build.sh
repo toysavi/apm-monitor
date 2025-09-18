cb18a8ad9c8571c033af8f4e300669bfd65d27f5
Toy Savi <savi.toy@amkcambodia.com>
2025-09-18 14:38:32 +0700

    update noted

--- build.sh
+++ build.sh
@@ -4,13 +4,19 @@ set -e
 NAMESPACE=monitoring
 
 echo "✅ Applying TLS secret..."
-kubectl apply -f build/build-tls.sh
+kubectl create secret tls kibana-tls \
+  --cert=certs/tls.crt \
+  --key=certs/tls.key \
+  -n $NAMESPACE \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "✅ Applying Elastic credentials secret..."
 kubectl apply -f build/elastic-secret.yaml
 
 echo "✅ Installing Ingress-NGINX..."
-kubectl apply -f build/ingress-nginx.sh
+sudo build/ingress-nginx.sh
 
 echo "✅ Applying MetalLB configuration..."
 kubectl apply -f build/metallb-system.yaml
 
-echo "✅ Build step completed."
+echo "✅ Build step completed
--- build/build-tls.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-kubectl create secret tls elastic-tls \
-  --cert=./ssl/cert.crt \
-  --key=./ssl/cert.key \
-  -n monitoring
