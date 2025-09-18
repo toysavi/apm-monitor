1d3ff5cd30c791079a98b9d8907134551b60a39a
Toy Savi <savi.toy@amkcambodia.com>
2025-09-18 14:50:07 +0700

    update noted

--- build.sh
+++ build.sh
@@ -3,6 +3,20 @@ set -e
 
 NAMESPACE=monitoring
 
+# List of namespaces to ensure exist
+namespaces=("ingress-nginx" "metallb-system" "monitoring")
+
+for ns in "${namespaces[@]}"; do
+    if kubectl get namespace "$ns" >/dev/null 2>&1; then
+        echo "Namespace $ns already exists, skipping..."
+    else
+        echo "Creating namespace $ns..."
+        kubectl create namespace "$ns"
+    fi
+done
+
+
+
 echo "✅ Applying TLS secret..."
 kubectl create secret tls kibana-tls \
   --cert=certs/tls.crt \
