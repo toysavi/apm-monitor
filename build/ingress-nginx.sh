4fa4ca6389990f4887fcb27ae2a1401d41d97453
Toy Savi <savi.toy@amkcambodia.com>
2025-09-25 13:30:08 +0700

    update noted

--- build/ingress-nginx.sh
+++ build/ingress-nginx.sh
@@ -3,6 +3,6 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm install ingress-nginx ingress-nginx/ingress-nginx \
-  --namespace observability \
+  --namespace ingress-nginx \
   --set controller.service.type=LoadBalancer \
   --set controller.service.loadBalancerIP=10.0.0.5
